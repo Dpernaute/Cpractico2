fd393777953330e27112fd5fd6b3d35e5b30d804
Diego  Pernaute Gómez <diego@MBP-de-Diego.home>
2021-02-27 16:23:04 +0100

    new chaneges in playbooks

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -7,7 +7,7 @@ variable "location" {
 variable "vm_size" {
   type = string
   description = "Tamaño de la máquina virtual"
-  default = "Standard_D1_v2" # 3.5 GB, 1 CPU 
+  default = "Standard_D1_v2" # 4 GB, 1 CPU 
 }
 
 # Variables del despliegue automático
